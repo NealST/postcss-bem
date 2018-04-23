2450dae41b2ce9c6887792e07205e3cbd5f55a2e
mozheng <mozheng@mogujie.com>
2018-04-23 09:23:15 +0800

    fix parent selector find error

--- index.js
+++ index.js
@@ -1,42 +1,44 @@
+import { atRule } from 'postcss';
+
 const postcss = require('postcss')
 
 const schema = {
   component: {
+    alias: 'b',
     separator: '.'
   },
   descendent: {
+    alias: 'e',
     separator: '-'
   },
   modifier: {
+    alias: 'm',
     separator: '--'
   },
   when: {
+    alias: 'w',
     separator: '_'
   }
 }
 
 module.exports = postcss.plugin('postcss-bem', function (opts) {
-  function getAtruleSelector (atRule, paramName, parentSelector) {
+  function getAtruleSelector (atRule, paramName) {
     let selectorArray = [schema[atRule.name].separator, paramName]
-    if (parentSelector) {
-      return `${parentSelector}${selectorArray.join('')}`
-    } else {
-      let theParent = atRule.parent
-      while (theParent && theParent.type !== 'root') {
-        selectorArray.unshift(`${schema[theParent.name].separator}${theParent.params}`)
-        theParent = theParent.parent
-      }
-      return selectorArray.join('')
+    let theParent = atRule.parent
+    while (theParent && theParent.type !== 'root') {
+      selectorArray.unshift(`${schema[theParent.name].separator}${theParent.params}`)
+      theParent = theParent.parent
     }
+    return selectorArray.join('')
   }
 
-  function processAtrule (css, atRule, parentSelector) {
+  function processAtrule (css, atRule) {
     let ruleName = atRule.name
     if (!schema.hasOwnProperty(ruleName)) {
       throw new Error(`you have written an unsupported type of bem declaration ${ruleName}`)
     }
     let paramName = atRule.params
-    let ruleSelector = getAtruleSelector(atRule, paramName, parentSelector)
+    let ruleSelector = getAtruleSelector(atRule, paramName)
     let newRule = postcss.rule({
       selector: ruleSelector
     })
@@ -44,17 +46,18 @@ module.exports = postcss.plugin('postcss-bem', function (opts) {
       if (item.type === 'decl') {
         newRule.append(item)
       }
-      if (item.type === 'atrule') {
-        processAtrule(css, item, ruleSelector)
-      }
     })
     css.append(newRule)
-    atRule.remove()
   }
 
   return function (css, result) {
+    let atRules = []
     css.walkAtRules(function (atRule) {
+      atRules.push(atRule)
       processAtrule(css, atRule)
+    })
+    atRule.forEach((item) => {
+      item.remove()
     })
   }
 })
